d29b6ba70482f317fec00e59d197e5a12086c798
Egire2 <e.spassky@sh.dunice.net>
2024-09-19 14:16:49 +0300

    Task solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Task;
+
+int[] arr = { 1, 2, 3 };
+byte[] arr2 = { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0 };
+var a = new ByteStreamReverse();
+//Console.WriteLine(a.GetIndiciesOfNumsThatSumToTarget(arr, 4));
+foreach (var item in a.ReverseArrayByte(arr2))
+{
+    Console.Write($"{item},");
+}
+Console.ReadLine();
